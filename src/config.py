8463ee66c23d858f5a1461dd92b5c96bbeed5ec8
edwardmcquaid1 <psyem7@nottingham.ac.uk>
2026-07-06 14:30:23 +0100

    Changed the way config.py handles filepaths

--- src/config.py
+++ src/config.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
-class FilePaths:
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    INPUT_DIR = BASE_DIR / "input"
-    OUTPUT_DIR = BASE_DIR / "output"
-    SRC_DIR = BASE_DIR / "src"
+BASE_DIR = Path(__file__).resolve().parent.parent
+INPUT_DIR = BASE_DIR / "input"
+OUTPUT_DIR = BASE_DIR / "output"
+SRC_DIR = BASE_DIR / "src"
--- src/main.py
+++ src/main.py
@@ -1,4 +1,1 @@
 from data.build_db import DataBase
-
-db = DataBase("assignments.db")
-db.load("data/assignments.csv")
